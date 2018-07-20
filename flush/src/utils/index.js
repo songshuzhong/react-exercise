378b72a0639a5ae0c0b15f0ee5e05c4d485e93eb
songshuzhong <sshuzhong@outlook.com>
2018-07-20 21:43:29 +0800

    feat: update
    - nothing

--- flush/src/utils/index.js
+++
@@ -1,9 +0,0 @@
-export const pages = {};
-
-export const nextIndex = index => ++index % pages.length;
-
-export const indexFromPath = path => {
-  path = path === '/'? '/foo': path;
-
-  return pages.indexOf( path.substr( 1 ) );
-};

163b04d2f5474d7d291eb49f85201bc6c8d23fca
songshuzhong <songshuzhong@baidu.com>
2019-05-20 11:11:37 +0800

    feat(): asdf

--- src/page/agreement/index.js
+++ src/page/agreement/index.js
@@ -5,19 +5,22 @@
  *@Date 2019/04/07
  *@desc 创作者注册
  */
-import * as React from 'react';
-import {render} from 'react-dom';
-import {HashRouter, Route, Switch} from 'react-router-dom';
+import * as React from 'react'
+import { render } from 'react-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 
-import {asyncRouter} from '../../components/asyncRouter';
+import { asyncRouter } from '../../components/asyncRouter'
 
 render(
-    <HashRouter>
-        <Switch>
-            <Route path='/' component={asyncRouter(() => import('./scene/home'))}/>
-            <Route path='/app' component={asyncRouter(() => import('./scene/app'))}/>
-            <Route path='/about' component={asyncRouter(() => import('./scene/about'))}/>
-        </Switch>
-    </HashRouter>,
-    document.getElementById('root')
-);
+  <HashRouter>
+    <Switch>
+      <Route path="/" component={asyncRouter(() => import('./scene/home'))} />
+      <Route path="/app" component={asyncRouter(() => import('./scene/app'))} />
+      <Route
+        path="/about"
+        component={asyncRouter(() => import('./scene/about'))}
+      />
+    </Switch>
+  </HashRouter>,
+  document.getElementById('root')
+)
